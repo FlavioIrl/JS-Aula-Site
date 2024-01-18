fef38e3b709bf789a73d4b5035c57bc95a7d1025
Flavio Irala <flavioiralap@gmail.com>
2024-01-17 20:16:12 -0400

    commit inicial

---
+++ main.js
@@ -0,0 +1,25 @@
+const form = document.getElementById('form-deposito');
+
+function validaNome(nomeCompleto) {
+    const nomeComoArray = nomeCompleto.split(' ');
+    return nomeComoArray.length >= 2;
+}
+
+form.addEventListener('submit', function(e) {
+    let formEvalido = false
+    e.preventDefault();
+
+    const nomeBeneficiario = document.getElementById ('nome-beneficiario');
+    const numeroContaBeneficiario = document.getElementById('numero-conta');
+    const valorDeposito = document.getElementById('valor-deposito');
+    const mensagemSucesso = `Montante de: ${valorDeposito} depositado para o cliente: ${nomeBeneficiario}- conta: ${numeroContaBeneficiario}`;
+
+    formEValido = validaNome(nomeBeneficiario.value)
+    if (formEValido) {
+        alert(mensagemSucesso);
+    } else {
+        alert("O nome não está completo!");
+    }
+})
+
+console.log(form);
